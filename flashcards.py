a113f9a78208d0a25a68f12ae5d54cf0aa31a7f2
ANKIT BHARDWAJ <bhankit1410@gmail.com>
2020-05-17 21:21:11 +0530

    count views

--- flashcards.py
+++ flashcards.py
@@ -3,12 +3,21 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+count_view =0
+
 
 @app.route("/")
 def welcome():
     return "welcome"
 
+
 @app.route("/date")
 def date():
     return "The time is " + str(datetime.now())
 
+
+@app.route("/count_views")
+def count_views():
+    global count_view
+    count_view += 1
+    return f"Welcome for the {count_view}th time"
