50564288a914b0ee3674c81bcff66f6bc55272ad
ANKIT BHARDWAJ <bhankit1410@gmail.com>
2020-05-17 20:09:52 +0530

    current date

--- flashcards.py
+++ flashcards.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def welcome():
-    return "welcome to my flash card app"
+    return "welcome"
+
+@app.route("/date")
+def date():
+    return "The time is " + str(datetime.now())
+
