d5f61fac7ac9878d13c5c0adfb6d334ad22b353d
ANKIT BHARDWAJ <bhankit1410@gmail.com>
2020-05-18 08:20:53 +0530

    added dynamic MVC in templates, removed simple views

--- flashcards.py
+++ flashcards.py
@@ -1,24 +1,8 @@
-from datetime import datetime
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
-count_view = 0
 
 
 @app.route("/")
 def welcome():
-    return "welcome"
-
-
-@app.route("/date")
-def date():
-    return "The time is " + str(datetime.now())
-
-
-@app.route("/count_views")
-def count_views():
-    global count_view
-    count_view += 1
-    return f"Welcome for the {count_view}th time"
+    return render_template("welcome.html", message="this is coming from any source")
